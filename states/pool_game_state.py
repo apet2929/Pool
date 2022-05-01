7d7beda1c31b9b9945c0d88cacfaeeb919a9e2a9
Andrew Petersen <realandrewpetersen@gmail.com>
2022-05-01 12:18:47 -0500

    Cleaned code, changed table and ball sizes, colors

--- states/pool_game_state.py
+++ states/pool_game_state.py
@@ -3,24 +3,21 @@ import math
 from assets import AssetCache
 from entitites.entity import Entity
 from states.state import State
-from pygame import Rect, Vector2
+from pygame import Rect, Surface, Vector2
 import pygame
 
 from utils import BASE_SIZE, check_sides, circle_intersection, is_circle_inside_circle, normalize_angle_rads
 
 class Ball(Entity):
     FRICTION = 0.02
-    RADIUS = 15
+    RADIUS = 10
     HOLE_RADIUS = 20
-    def __init__(self, image: pygame.Surface, position, mass=1) -> None:
-        super().__init__(image, (Ball.RADIUS * 2, Ball.RADIUS * 2), position)
+    TABLE_SIZE = [BASE_SIZE[0], BASE_SIZE[0]/2]
+    def __init__(self, color, position, mass=1) -> None:
+        super().__init__(draw_ball(color), (Ball.RADIUS * 2, Ball.RADIUS * 2), position)
         self.mass: float = mass
         self.velocity: Vector2 = Vector2(0,0)
         self.forces: list[Vector2] = []
-        
-    def render(self, screen):
-        # super().render(screen)
-        pygame.draw.circle(screen, (255,0,0), self.rect.center, Ball.RADIUS)
 
     def update(self, delta, **kwargs):
         self.apply_friction()
@@ -49,8 +46,8 @@ class Ball(Entity):
         current_time = delta
         intersected = True
         while current_time > -(delta * 3) and intersected:
-            self_cur_pos = self.get_pos_at_time(current_time)
-            other_cur_pos = ball.get_pos_at_time(current_time)
+            self_cur_pos = self.get_pos_after_time(current_time)
+            other_cur_pos = ball.get_pos_after_time(current_time)
             if circle_intersection(self_cur_pos, other_cur_pos, Ball.RADIUS):
                 # Continue
                 current_time -= delta * 0.2
@@ -59,7 +56,7 @@ class Ball(Entity):
                 return current_time
         print("Sweep failed! No time found where intersection absent")    
 
-    def get_pos_at_time(self, delta) -> Vector2:
+    def get_pos_after_time(self, delta) -> Vector2:
         return self.position + (self.velocity * delta)
 
     def check_sunk(self, holes: list[tuple]) -> bool:
@@ -97,30 +94,32 @@ class Ball(Entity):
 
     def collide_balls(self, delta, balls):
             # Sweep through time to find the initial intersection point
-            future = self.get_pos_at_time(delta)
-            future_rect = self.get_rect()
-            future_rect.center = [future.x, future.y]
+            future = self.get_pos_after_time(delta)
             for ball in balls:
                 if ball == self:
                     continue
-                ball_future = ball.get_pos_at_time(delta)
-                ball_rect = ball.get_rect()
-                ball_rect.center = [ball_future.x, ball_future.y]
-                if future_rect.colliderect(ball_rect): # Fast rectangle collision test
-                    # Perform a better test
-                    if circle_intersection(future, ball.get_pos_at_time(delta), Ball.RADIUS):
-                        # Sweep back in time
+                if self.check_collided(ball, future, delta):
+                    intersected_time = self.get_intersection_time(ball, delta)
+                    if(intersected_time is None):
+                        intersected_time = 5 * delta # TODO : Find a way to kick balls out that are slowly intersecting
+                    self.position = self.get_pos_after_time(intersected_time)
+                    ball.position = ball.get_pos_after_time(intersected_time)
+                    self.collide_ball(ball)
 
-                        intersected_time = self.sweep_circle(delta, ball) # Potentially really expensive, we'll see!
+    def check_collided(self, ball, future_pos, delta) -> bool:
+        future_rect = self.get_rect()
+        future_rect.center = [future_pos.x, future_pos.y]
+        ball_future = ball.get_pos_after_time(delta)
+        ball_rect = ball.get_rect()
+        ball_rect.center = [ball_future.x, ball_future.y]
+        if future_rect.colliderect(ball_rect): # Fast rectangle collision test
+            return circle_intersection(future_pos, ball.get_pos_after_time(delta), Ball.RADIUS)
+        return False
                         
-                        if(intersected_time is None):
-                            intersected_time = 5 * delta
-                        self.position = self.get_pos_at_time(intersected_time)
-                        ball.position = ball.get_pos_at_time(intersected_time)
-                        self.collide_ball(ball, intersected_time)
-                        
-                            
-    def collide_ball(self, other, dt):
+    def get_intersection_time(self, ball, delta) -> float or None:
+        return self.sweep_circle(delta, ball) # Potentially really expensive, we'll see!
+                                    
+    def collide_ball(self, other):
         difference: Vector2 = self.position - other.position
         unit_normal = difference.normalize()
         unit_tangent = Vector2(-unit_normal.y, unit_normal.x)
@@ -160,6 +159,12 @@ class Ball(Entity):
         r.centery = self.position.y
         return r
 
+def draw_ball(color) -> Surface:
+    surf = pygame.Surface((Ball.RADIUS * 2, Ball.RADIUS * 2))
+    pygame.draw.circle(surf, color, surf.get_rect().center, Ball.RADIUS)
+    surf.set_colorkey(surf.get_at((0,0)))
+    return surf
+
 class PoolGameState(State):
     def __init__(self) -> None:
         super().__init__()
@@ -167,7 +172,10 @@ class PoolGameState(State):
         self.aim: Vector2 = None
         self.walls: list[Rect] = []
         self.holes: list[tuple] = []
-        self.bg_color = (42, 102, 55)
+
+        self.table = Rect(10, (BASE_SIZE[1] - Ball.TABLE_SIZE[1]) / 2, Ball.TABLE_SIZE[0], Ball.TABLE_SIZE[1])
+        self.table_color = (42, 102, 55)
+        self.bg_color = (80, 101, 148)
         self.wall_color = (79, 51, 16)
 
     def on_enter(self, ass_cache: AssetCache):
@@ -176,7 +184,7 @@ class PoolGameState(State):
         self.init_holes()
 
     def render(self, screen: pygame.Surface):
-        screen.fill(self.bg_color)
+        self.draw_background(screen)
 
         self.draw_holes(screen)
         for ball in self.balls:
@@ -220,37 +228,36 @@ class PoolGameState(State):
 
     def init_walls(self):
         width = 10
-        self.walls.append(Rect(0,0,BASE_SIZE[0], width)) # top
-        self.walls.append(Rect(0,BASE_SIZE[1] - width,BASE_SIZE[0], width)) # bottom
-        self.walls.append(Rect(0,0,width, BASE_SIZE[1])) # left
-        self.walls.append(Rect(BASE_SIZE[0]-width,0,width, BASE_SIZE[1])) # right
+        tw = Ball.TABLE_SIZE[0]
+        th = Ball.TABLE_SIZE[1]
+        margin_top = (BASE_SIZE[1] - th) / 2
+        self.walls.append(Rect(0,margin_top,tw, width)) # top
+        self.walls.append(Rect(0,th - width + margin_top,tw, width)) # bottom
+        self.walls.append(Rect(0,margin_top,width, th)) # left
+        self.walls.append(Rect(tw-width,margin_top,width, th)) # right
 
     def init_balls(self, ass_cache):
         w = BASE_SIZE[0]
         h = BASE_SIZE[1]
         r = Ball.RADIUS
         d = Ball.RADIUS * 2
-        self.balls.append(Ball(ass_cache.get_asset("CropSprite"), (w/4,h/2), 1.005)) 
         # self.balls.append(Ball(ass_cache.get_asset("CropSprite"), (400,200), 1))
         balls = [
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4,h/2)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + d,h/2 + r)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + d,h/2 - r)),
+            Ball((242, 230, 216), (w/4,h/2), 1.005), #cue ball
 
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 2*d, h/2)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 2*d, h/2 - d)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 2*d, h/2 + d)),
+            Ball((242, 224, 82), (3*w/4,h/2)),
 
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 3*d, h/2 + r)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 3*d, h/2 - r)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 3*d, h/2 + d+r)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 3*d, h/2 - d-r)),
+            Ball((112, 156, 255), (3*w/4 + d,h/2 + r)),
+            Ball((217, 41, 214), (3*w/4 + d,h/2 - r)),
 
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 4*d, h/2)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 4*d, h/2 + d)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 4*d, h/2 - d)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 4*d, h/2 + 2*d)),
-            Ball(ass_cache.get_asset("CropSprite"), (3*w/4 + 4*d, h/2 - 2*d)),
+            Ball((72, 4, 110), (3*w/4 + 2*d, h/2)),
+            Ball((242, 235, 99), (3*w/4 + 2*d, h/2 - d)),
+            Ball((232, 162, 32), (3*w/4 + 2*d, h/2 + d)),
+
+            Ball((68, 161, 42), (3*w/4 + 3*d, h/2 + r)),
+            Ball((176, 36, 32), (3*w/4 + 3*d, h/2 - r)),
+
+            Ball((38, 8, 7), (3*w/4 + 4*d, h/2)),
         ]
         self.balls.extend(balls)
     
@@ -258,11 +265,12 @@ class PoolGameState(State):
         r = Ball.HOLE_RADIUS
         margin = 5
         rm = r + margin
-        w = BASE_SIZE[0]
-        h = BASE_SIZE[1]
+        w = Ball.TABLE_SIZE[0]
+        h = Ball.TABLE_SIZE[1]
+        tm = (BASE_SIZE[1] - h)/2
         self.holes = [
-            (rm, rm), (w - rm, rm), (w - rm, h - rm), (rm, h - rm),
-            (w/2, rm), (w/2, h - rm)
+            (rm, rm + tm), (w - rm, rm + tm), (w - rm, h - rm + tm), (rm, h - rm + tm),
+            (w/2, rm + tm), (w/2, h - rm + tm)
         ]
 
     def draw_walls(self, screen):
@@ -272,6 +280,10 @@ class PoolGameState(State):
     def draw_holes(self, screen):
         for hole in self.holes:
             pygame.draw.circle(screen, (0,0,0), hole, Ball.HOLE_RADIUS)
+
+    def draw_background(self, screen: Surface):
+        screen.fill(self.bg_color)
+        screen.fill(self.table_color, self.table)
 
     def run_test(self, launch_angle):
         a = math.radians(launch_angle)
